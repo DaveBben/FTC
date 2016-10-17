574a0ec99c69a1dfb82bebe7164e338a4df51f0b
movieboy711 <david.a.bennett7@gmail.com>
2016-10-17 19:33:39 -0400

    changed stuff

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
-var questionOne = 'Tell a professor that someone in class is cheating';
-var questionTwo = 'Tell your boyfriend or girlfriend that you no longer want to be a couple';
-var questionThree = 'Running into a building on fire to save a child';
-var questionFour = 'Endure debilitating, chronic pain due to cancer';
-var questionFive = 'Forgive a friend who committed a serious wrong against you';
-var questionSix = 'Tell your conservative parents that you are gay';
-var questionSeven ='Go to an Alcholics Anonymous meeting for the first time';
-var questionEight = 'Confront a friend who you know is stealing money from housemate';
-var highQ1, highQ2, highQ3, highQ4;
-var winenrA, winnerB, winnerC;
+
+<?php
+
+
+$questionOne = 'Tell a professor that someone in class is cheating';
+$questionTwo = 'Tell your boyfriend or girlfriend that you no longer want to be a couple';
+$questionThree = 'Running into a building on fire to save a child';
+$questionFour = 'Endure debilitating, chronic pain due to cancer';
+$questionFive = 'Forgive a friend who committed a serious wrong against you';
+$questionSix = 'Tell your conservative parents that you are gay';
+$questionSeven ='Go to an Alcholics Anonymous meeting for the first time';
+$questionEight = 'Confront a friend who you know is stealing money from housemate';
+$highQ1, highQ2, highQ3, highQ4;
+$winenrA, winnerB, winnerC;
 	
-var counter = 1;
-var selected;
+$counter = 1;
+$selected;
 
 function getQuestion(id){
 	switch(id){
@@ -66,7 +70,6 @@ function getID(question){
 }
 	
 function display(idOne,idTwo){
-	fade();
 	document.getElementById('questionOne').innerHTML = getQuestion(idOne);
 	document.getElementById('containerOne').value = getID(getQuestion(idOne));
 	document.getElementById('questionTwo').innerHTML = getQuestion(idTwo);
@@ -120,8 +123,57 @@ function change(answer){
 }
 
 
-function fade(){
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Find the Courage</title>
+  <meta name="Widener University: Find the Courage" content="Find the Courage Survey">
+  <meta name="Dave Bennett & Richie Le" content="Find the Courage">
+  <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="css/style.css">
+ 
+</head>
+
+<body onload="display(0,1)">
+	<div id = "MainDiv">
+		<div id="containerOne" onclick="change(this.value)">
+			<p id="questionOne">
+				This is the first Questions
+			</p>
+		</div>
+		<br>
+		<div id="containterTwo" onclick="change(this.value)">
+			<p id="questionTwo">
+				This is the second Question
+			</p>
+		</div>
+   </div>
+   	
+	<script type="text/javascript" src="js/script.js"></script>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	
+	<script>
 		$(function(){
 		  $('#MainDiv').hide().fadeIn('slow');
 		});
-}
+
+
+		$( "#containerOne" ).click(function() {
+		  $('#MainDiv').hide().fadeIn('slow');
+		});
+
+
+
+		$( "#containterTwo" ).click(function() {
+		  $('#MainDiv').hide().fadeIn('slow');
+		});
+</script>
+</body>
+
+<footer> Made by Dave Bennett and Richie Le </footer>
+</html>
